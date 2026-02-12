cdb5e5d909063b2ded0de21c6c2bfe1155800938
sahil <sahil9955555@gmail.com>
2026-02-12 17:53:31 +0530

    User Entity added

---
+++ src/main/java/com/sahil/springsecuritydemo/entity/User.java
@@ -0,0 +1,34 @@
+package com.sahil.springsecuritydemo.entity;
+
+import java.util.Set;
+
+import com.sahil.springsecuritydemo.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<Role> roles;
+
+}
